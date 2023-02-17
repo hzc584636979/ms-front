f928583324ce830532441b4e536b334c32b42873
houzicheng <houzicheng@rskjy.cn>
2023-02-17 15:59:24 +0800

    1

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://test-oss-api.rskjy.cn/course/api'
+  window.SITE_CONFIG['baseUrl'] = ''
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
--- static/config/index.js
+++ static/config/index.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://test-oss-api.rskjy.cn/course/api' // 测试环境
+  window.SITE_CONFIG['baseUrl'] = '' // 测试环境
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
