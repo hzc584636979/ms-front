73fdbb90c13b87cb53631b98e34dcbbc5377d4bc
houzicheng <houzicheng@rskjy.cn>
2023-03-16 15:37:29 +0800

    1

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,15 +1,19 @@
 'use strict'
 
-let baseUrl = ''
+let webOps = {
+  BASE_NAME: '"' + process.env.npm_config_website + '"',
+  BASE_FAVICON: '"static/img/favicon_' + process.env.npm_config_website + '.png"'
+}
 if (process.env.npm_config_website == 'nc') {
-  baseUrl = '"http://124.71.46.129:880/api"'
+  webOps.BASE_URL = '"http://124.71.46.129:880/api"'
+  webOps.BASE_TITLE = '"墨水激活系统 Ink Activation System"'
 } else {
-  baseUrl = '"http://' + process.env.npm_config_website + ':880/api"'
+  webOps.BASE_URL = '"http://' + process.env.npm_config_website + ':880/api"'
+  webOps.BASE_TITLE = '"' + process.env.npm_config_website + '墨水激活系统 Ink Activation System"'
 }
 
 module.exports = {
   NODE_ENV: '"development"',
-  BASE_NAME: '"' + process.env.npm_config_website + '"',
-  BASE_URL: baseUrl,
   OPEN_PROXY: false, // 是否开启代理, 重置后需重启vue-cli
+  ...webOps
 }
