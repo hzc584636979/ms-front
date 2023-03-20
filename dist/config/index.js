3a50e5a896980813b791fbd8a855bfc9b19b5dc6
houzicheng <houzicheng@rskjy.cn>
2023-03-20 11:30:29 +0800

    1

--- dist/config/index.js
+++ dist/config/index.js
@@ -7,7 +7,7 @@
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['siteName'] = 'mt' // 编译平台
   window.SITE_CONFIG['domain'] = './' // 域名
-  window.SITE_CONFIG['version'] = '2303182100'   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '2303201126'   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
 })()
 
