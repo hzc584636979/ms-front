89adc452ffd05e040123a0e9d39121d965464381
houzicheng <houzicheng@rskjy.cn>
2023-05-29 13:07:34 +0800

    1

--- dist/config/index.js
+++ dist/config/index.js
@@ -5,9 +5,9 @@
   window.SITE_CONFIG = {}
 
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['siteName'] = 'nc' // 编译平台
+  window.SITE_CONFIG['siteName'] = 'mt' // 编译平台
   window.SITE_CONFIG['domain'] = './' // 域名
-  window.SITE_CONFIG['version'] = '2303201738'   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '2305291306'   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version + '_' + window.SITE_CONFIG['siteName']
 })()
 
