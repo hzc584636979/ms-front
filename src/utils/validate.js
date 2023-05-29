bba1e1945ab6e93d327754cbd1ccc5f138823725
houzicheng <houzicheng@rskjy.cn>
2023-05-29 13:33:58 +0800

    1

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,7 +3,7 @@
  * @param {*} s
  */
 export function isEmail (s) {
-  // return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s)
+  // return /^[a-zA-Z0-9]+([-_.][A-Za-zd]+)*@([a-zA-Z0-9]+[-.])+[A-Za-zd]{2,5}$/.test(s)
   return s
 }
 
